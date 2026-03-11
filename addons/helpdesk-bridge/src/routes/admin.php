5314f26615986bee9e07943e754ab6c2c142a870
FreezMod <freezmod.dev@gmail.com>
2026-03-11 19:51:32 +0100

    Remove helpdesk addon

--- addons/helpdesk-bridge/src/routes/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Addons\HelpdeskBridge\Http\Controllers\Admin\SettingsController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [SettingsController::class, 'index'])->name('index');
-Route::put('/', [SettingsController::class, 'update'])->name('update');
