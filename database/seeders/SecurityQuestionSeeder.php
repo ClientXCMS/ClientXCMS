f4fbd31c4482262cd2f84be48ff12cc38e5e7aba
FreezMod <69022745+TomLeDev@users.noreply.github.com>
2026-03-11 21:01:01 +0100

    Fix SecurityQuestionSeeder void return fatal error

--- database/seeders/SecurityQuestionSeeder.php
+++ database/seeders/SecurityQuestionSeeder.php
@@ -20,9 +20,7 @@
 namespace Database\Seeders;
 
 use App\Models\Admin\SecurityQuestion;
-use App\Models\Personalization\Translation;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Schema;
 
 class SecurityQuestionSeeder extends Seeder
 {
@@ -45,24 +43,11 @@ class SecurityQuestionSeeder extends Seeder
         ];
 
         foreach ($questions as $question) {
-            $securityQuestion = SecurityQuestion::create([
+            SecurityQuestion::create([
                 'question' => $question['question'],
                 'is_active' => true,
                 'sort_order' => $question['sort_order'],
             ]);
-
-            foreach ($question['translations'] as $locale => $content) {
-                Translation::updateOrCreate([
-                    'model' => SecurityQuestion::class,
-                    'model_id' => $securityQuestion->id,
-                    'key' => 'question',
-                    'locale' => $locale,
-                ], [
-                    'content' => $content,
-                ]);
-            }
         }
-
-        return collect($locales)->mapWithKeys(fn (string $locale) => [$locale => $question])->toArray();
     }
 }
