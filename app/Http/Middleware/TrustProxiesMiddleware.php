9427b3ff12e406e483384451ed8ccf9c05de3fc0
Martin DELEBECQUE <delebecque.martin@gmail.com>
2026-04-04 18:37:24 +0200

    refactor: improve proxy management

--- app/Http/Middleware/TrustProxiesMiddleware.php
+++ app/Http/Middleware/TrustProxiesMiddleware.php
@@ -26,31 +26,6 @@ use URL;
 
 class TrustProxiesMiddleware extends TrustProxies
 {
-    protected $proxies = [
-        '103.21.244.0/22',
-        '103.22.200.0/22',
-        '103.31.4.0/22',
-        '104.16.0.0/13',
-        '104.24.0.0/14',
-        '108.162.192.0/18',
-        '131.0.72.0/22',
-        '141.101.64.0/18',
-        '162.158.0.0/15',
-        '172.64.0.0/13',
-        '173.245.48.0/20',
-        '188.114.96.0/20',
-        '190.93.240.0/20',
-        '197.234.240.0/22',
-        '198.41.128.0/17',
-        '2400:cb00::/32',
-        '2606:4700::/32',
-        '2803:f800::/32',
-        '2405:b500::/32',
-        '2405:8100::/32',
-        '2a06:98c0::/29',
-        '2c0f:f248::/32',
-    ];
-
     /**
      * Handle an incoming request.
      *
@@ -59,7 +34,7 @@ class TrustProxiesMiddleware extends TrustProxies
     public function handle(Request $request, Closure $next): Response
     {
         $request->headers->set('X-Forwarded-For', $request->header('CF-Connecting-IP'));
-        Request::setTrustedProxies($this->proxies, $this->headers);
+        Request::setTrustedProxies($this->getProxies(), $this->headers);
         if (! $request->secure()) {
             $this->setProtocolForRequest($request);
         }
@@ -88,5 +63,14 @@ class TrustProxiesMiddleware extends TrustProxies
         if ($cfVisitor->scheme === 'https' && ! $request->secure()) {
             $request->server->set('HTTPS', 'on');
         }
+    }
+
+    protected function getProxies(): array
+    {
+        if (file_exists(storage_path('app/proxies.json'))) {
+            return json_decode(file_get_contents(storage_path('app/proxies.json')), true);
+        }
+
+        return config('app.proxies');
     }
 }
