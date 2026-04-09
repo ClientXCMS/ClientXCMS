e4d72e3b4e09a82e73f5611625978c10f9bcfcde
Martin DELEBECQUE <delebecque.martin@gmail.com>
2026-04-09 23:41:58 +0200

    fix: resolve BillingServiceProvider signature mismatch in booted method

--- tests/Feature/Client/EmailControllerTest.php
+++ tests/Feature/Client/EmailControllerTest.php
@@ -76,6 +76,8 @@ class EmailControllerTest extends TestCase
         Customer::factory(15)->create();
         EmailMessage::factory(15)->create();
         $user = $this->createCustomerModel();
+        // attach user to all emails
+        EmailMessage::where('recipient_id', null)->update(['recipient_id' => $user->id]);
         $this->actingAs($user)->get(route('front.emails.read-all'))->assertRedirect();
         $this->assertNotNull(EmailMessage::where('recipient_id', $user->id)->whereNotNull('read_at')->first());
     }
